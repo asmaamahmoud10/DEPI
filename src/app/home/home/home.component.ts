338f9ed32f197488f7f19629ac505f5884a75e01
Mai Gamal Salah <maigamal86316@gmail.com>
2025-04-13 17:41:04 +0100

    add login

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/notes/note.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html'
 })
-export class HomeComponent {
-  notes= [];
+export class HomeComponent implements OnInit {
+  notes: any[] = [];
+
+  constructor(private noteService: NoteService) {}
+
+  ngOnInit() {
+    this.noteService.getNotes().subscribe((data) => {
+      this.notes = data;
+    });
+  }
 }
